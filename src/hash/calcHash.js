a94beebfa2ef5585f794ef407c435bb794761728
Peter Markushin <p.markushin@itransition.com>
2022-12-03 22:38:45 +0300

    feat: Add hash task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { open } from 'node:fs/promises';
+import { stdout } from 'node:process';
+const { createHash } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileName = join(
+        dirname(fileURLToPath(import.meta.url)),
+        'files',
+        'fileToCalculateHashFor.txt'
+    );
+    const hash = createHash('sha256');
+    const fd = await open(fileName);
+    const input = fd.createReadStream({encoding: 'utf8'});
+
+    input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
