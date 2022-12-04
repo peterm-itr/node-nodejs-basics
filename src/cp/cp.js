0e509e44d3547fa20b2e2495cc7ff3410a667149
Peter Markushin <p.markushin@itransition.com>
2022-12-04 23:54:36 +0300

    feat: Add child process task solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { fork } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptFilePath = join(dirname(fileURLToPath(import.meta.url)), 'files', 'script.js');
+    const child = fork(scriptFilePath, args, {stdio: ['inherit', 'inherit', 'inherit', 'ipc']});
+
+    child.on('error', (err) => console.error('Fork failed', err));
+
+    return child;
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(process.argv.slice(2));
